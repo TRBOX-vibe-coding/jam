7d53d2befaa56ffb4de2e940fa550ba16d03e6e0
TRBOX-vibe-coding <302329444+TRBOX-vibe-coding@users.noreply.github.com>
2026-07-27 14:24:00 +0900

    Cloudflare Pages(jam) 배포 방식으로 전환한다.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AppProvider } from './context/AppContext'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
